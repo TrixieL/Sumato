24e35cd2a1b55b161dfe60ae311b6834bef0b701
elkbot <elkbot@users.noreply.github.com>
2016-04-15 12:35:42 +0200

    TCPConnection can listen for messages (needs more fine-tuning)

--- app/Assets/Scripts/TCPConnection.cs
+++ app/Assets/Scripts/TCPConnection.cs
@@ -1,28 +1,97 @@
 ﻿using UnityEngine;
-using System.Collections;
+using UnityEngine.UI;
 using System.Net.Sockets;
 using System.IO;
+using System.Threading;
+using System.Collections.Generic;
+using System;
 
-public class TCPConnection : MonoBehaviour {
-
-	TcpClient client;
-
+public class TCPConnection : MonoBehaviour
+{
 	public string server;
 	public int port;
 
-	public void Connect() {
+	TcpClient client;
+	NetworkStream stream;
+	StreamWriter writer;
+	StreamReader reader;
 
+	bool listening;
+	Thread listeningThread;
+
+	Queue<string> pendingMessages = new Queue <string> ();
+
+	/*
+	 * Begins a new TCP connection. Assign the server and port in the Unity UI.
+	 */
+	public void connectClient ()
+	{
 		client = new TcpClient (server, port);
-
+		writer = new StreamWriter (client.GetStream());
+		reader = new StreamReader (client.GetStream());
 	}
 
-	public void Send(string message) {
+	/*
+	 * Start the listening thread.  
+	 */
+	public void startListening ()
+	{
+		listeningThread = new Thread (read);
+		listeningThread.Start ();
+		listening = true;
+	}
 
-		using (StreamWriter streamWriter = new StreamWriter (client.GetStream ())) {
-			streamWriter.WriteLine (message);
-			streamWriter.Flush ();
+	/*
+	 * Send a string through the client.  
+	*/
+	public void send (string message)
+	{
+		writer.WriteLine (message);
+		writer.Flush ();
+	}
+
+	/*
+	 * Close the connection and the listening thread.  
+	 */
+	public void close ()
+	{
+		if (client != null) {
+			client.GetStream ().Close ();
+			client.Close ();
+		}
+		listening = false;
+		if (listeningThread.IsAlive) {
+			listeningThread.Abort ();
+		}
+	}
+
+	/*
+	 * Read data from the stream. Automatically started when the listening thread is started. 
+	 */
+	void read ()
+	{
+		while (listening) {
+			string msg = reader.ReadLine ();		
+			if (!String.IsNullOrEmpty (msg)) {
+				lock (pendingMessages) {
+					pendingMessages.Enqueue (msg);
+				}
+			}
+		}
+	}
+
+	/*
+	 * Unity method that runs once every frame.
+	 */
+	public void Update ()
+	{
+		//check for received data every frame
+		lock (pendingMessages) {
+			if (pendingMessages.Count > 0) {
+				string msg = pendingMessages.Dequeue ();
+				//process the message here or in a different method depending on what was received
+			}
 		}
 
 	}
-
 }
