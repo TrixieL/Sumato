6bf6e0a6b6c4d09151200c46b207414f48ea48b1
Ioannis Gkikas <funkyfisch@gmail.com>
2016-05-23 01:50:51 +0200

    Unified GUI with logic.sample apk is in app/build/outputs

--- app/Android Studio/Sumato/app/src/main/java/team10/sumato/ConnectActivity.java
+++ app/Android Studio/Sumato/app/src/main/java/team10/sumato/ConnectActivity.java
@@ -4,71 +4,101 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Gravity;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.Button;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 
 public class ConnectActivity extends AppCompatActivity {
 
-    Button btnConnect;
+    private WebView webGUI;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_connect);
+
+        webGUI = new WebView(this);
+        webGUI.setWebViewClient(new WebViewClient());
+        webGUI.loadUrl("file:///android_asset/index.html");
+        setContentView(webGUI);
+        webGUI.getSettings().setJavaScriptEnabled(true);
+        webGUI.getSettings().setDomStorageEnabled(true);
+        webGUI.addJavascriptInterface(new JSInterface(this,this), "Interface");
+    }
+
+
+    public void ConnectSumato(){
+        initSingleton();
+        //TCPSingleton.getInstance().getClient().start();
+       // TCPSingleton.getInstance().getClient().send("PAIR_PS3");
+
+        //TODO: check if the pairing was successful and move to the next page
+
+        // if (checkSingleton()) {
+            LoadPage("choice.html");
+        // }
+        //else {
+        //    Log.d("Connection was success","false");
+        //    Toast toast = Toast.makeText(this, "Ooops try again!", Toast.LENGTH_LONG);
+        //    toast.setGravity(Gravity.CENTER_VERTICAL, 80, 0);
+        //    toast.show();
+        //}
+
+    }
+
+    public void LoadPage(String page){
+        final String Page = page;
+        webGUI.post(new Runnable() {   //force the task (url loading) to run on the main thread because of WebView restrictions
+            @Override
+            public void run() {
+                webGUI.loadUrl("file:///android_asset/" + Page);
+            }
+        });
+    }
+
+    public void VRmode() {
+        Intent trackerActivity = new Intent(this, TrackingActivity.class);
+        startActivity(trackerActivity);
+    }
+
+    public void StandardMode(){
+        Intent intent = new Intent(ConnectActivity.this, StreamActivity.class);
+        startActivity(intent);
+    }
 
     protected void initSingleton(){
         SharedPreferences preferences = getSharedPreferences("team10.sumato_preferences", MODE_PRIVATE);
         TCPSingleton.initSingleton(preferences.getString("IP_key", "127.0.0.1"), preferences.getString("port_key", "9999"));
     }
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_connect);
-        btnConnect = (Button) findViewById(R.id.btnConnect);
-
-        btnConnect.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                connectSumato();
-            
-
-            }
-       });
-    }
-
-    public void connectSumato(){
-        initSingleton();
-        //TCPSingleton.getInstance().getClient().start();
-       // TCPSingleton.getInstance().getClient().send("PAIR_PS3");
-
-        //TODO: check if the pairing was successful and move to the next activity
-
-        Intent trackerActivity = new Intent(this, TrackingActivity.class);
-        startActivity(trackerActivity);
-        
-
-    }
-
-    public void streamVideo(View view){
-        Intent intent = new Intent(ConnectActivity.this, StreamActivity.class);
-        startActivity(intent);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.toolbar_menu, menu);
+    public boolean checkSingleton() {
+        //// TODO: 5/23/2016 check connection and if everything is in order(probably with a handshake).return the status.
         return true;
     }
 
-    /*
-     * Open the settings menu.
-     */
-    public void onGroupItemClick(MenuItem item){
-        startActivity(new Intent(getBaseContext(), SettingsActivity.class));
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus) {
+            getWindow().getDecorView().setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+        }
     }
-
-
-
-
 }
